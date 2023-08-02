aafc4349ff8715872996ea0ba2881d5884462bc4
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-08-02 17:30:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import App from 'components/App';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import GlobalStyle from 'styles/globalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <GlobalStyle />
+    <BrowserRouter basename="/test_sameit">
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>
 );
