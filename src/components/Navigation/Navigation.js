64664a3976d65bab2b12c04c531811f2f5ad2984
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-08-02 17:29:17 +0300

    Create Navigation.js

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,37 @@
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+  return (
+    <nav>
+      <NavLink
+        style={({ isActive }) => {
+          return {
+            display: 'inline-block',
+            padding: '12px',
+            fontWeight: '700',
+            color: isActive ? 'hsl(259, 52%, 55%)' : 'hsl(257, 46%, 27%)',
+          };
+        }}
+        to="/"
+      >
+        Parcels
+      </NavLink>
+
+      <NavLink
+        style={({ isActive }) => {
+          return {
+            display: 'inline-block',
+            padding: '12px',
+            fontWeight: '700',
+            color: isActive ? 'hsl(259, 52%, 55%)' : 'hsl(257, 46%, 27%)',
+          };
+        }}
+        to="/departments"
+      >
+        Departments
+      </NavLink>
+    </nav>
+  );
+};
+
+export default Navigation;
