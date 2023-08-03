eeabbbdf8b1862e2f07a4c018285ffbef221181a
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-08-03 21:01:19 +0300

    Create TrackingInfoBar.js

---
+++ src/components/TrackingInfoBar/TrackingInfoBar.js
@@ -0,0 +1,46 @@
+// import { Card, CardContent, Typography } from '@mui/material';
+// import { FC } from 'react';
+// import { useTracking } from '../redux/hooks';
+// import { PlaceholderText } from './PlaceholderText';
+
+import { ContentBox } from 'styles/Element.styled';
+
+// export const TrackInfo: FC = () => {
+//   const { data } = useTracking();
+
+//   return (
+//     <Card variant="outlined" sx={{ borderRadius: '16px' }}>
+//       <CardContent>
+//         {data ? (
+//           <>
+//             <Typography variant="h5" component="h2" gutterBottom>
+//               <span style={{ fontWeight: 'bold' }}>Status</span>: {data.Status}
+//             </Typography>
+//             <Typography variant="body1" component="p" gutterBottom>
+//               <span style={{ fontWeight: 'bold' }}>Warehouse sender</span>:{' '}
+//               {data.WarehouseSender}
+//             </Typography>
+//             <Typography variant="body1" component="p">
+//               <span style={{ fontWeight: 'bold' }}>Warehouse recipient</span>:{' '}
+//               {data.WarehouseRecipient}
+//             </Typography>
+//           </>
+//         ) : (
+//           <PlaceholderText
+//             message={
+//               'To display information about the parcel, enter TTN in the field above'
+//             }
+//           />
+//         )}
+//       </CardContent>
+//     </Card>
+//   );
+// };
+
+const TrackInfoBar = () => (
+  <ContentBox>
+    <p>Статус доставки...</p>
+  </ContentBox>
+);
+
+export default TrackInfoBar;
