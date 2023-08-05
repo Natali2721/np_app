3d405360b05ece4d3faa0914e09ec392a7c80327
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-08-05 20:29:03 +0300

    Update Parcels.js

--- src/pages/Parcels.js
+++ src/pages/Parcels.js
@@ -1,21 +1,68 @@
+import { getTrackingInfo } from 'Api/apiServices';
 import { SearchBar } from 'components/SearchBar/SearchBar';
 import TrackHistory from 'components/TrackingHistory/TrackingHistory';
 import TrackInfoBar from 'components/TrackingInfoBar/TrackingInfoBar';
+import { useEffect, useState } from 'react';
 import { Container, ContainerInfo } from 'styles/Element.styled';
 
 const Parcels = () => {
+  const [trackingList, setTrackingList] = useState([]);
+  const [trackingNumber, setTrackingNumber] = useState('');
+  const [trackingInfo, setTrackingInfo] = useState({});
+
   const handleSubmit = e => {
     e.preventDefault();
-    //const searchForm = e.currentTarget;
-    //setSearchParams({ movieName: searchForm.elements.movieName.value });
-    //searchForm.reset();
+    const searchForm = e.currentTarget;
+    console.log(searchForm.elements.TTN.value);
+    setTrackingNumber(searchForm.elements.TTN.value);
+    searchForm.reset();
   };
+
+  const onClick = () => {
+    setTrackingList([]);
+    setTrackingNumber('');
+  };
+
+  useEffect(() => {
+    if (trackingNumber === '') {
+      return console.log('There is no TTN number');
+    }
+    console.log(trackingNumber);
+
+    getTrackingInfo(trackingNumber).then(data => {
+      console.log(data);
+      if (!data) {
+        setTrackingInfo({});
+        return console.log(
+          'There is no TTN with this request. Please, try again'
+        );
+      }
+      setTrackingInfo(data);
+      //setTrackingList(prevState => [...prevState, data]);
+      if (!trackingList.includes(trackingNumber)) {
+        setTrackingList(prevState => [...prevState, trackingNumber]);
+      }
+    });
+  }, [trackingNumber]);
+
+  useEffect(() => {
+    if (trackingList.length < 1) {
+      setTrackingInfo({});
+    }
+  }, [trackingList]);
+
+  // useEffect(() => {
+  //   console.log(trackingInfo);
+  //   console.log(trackingList);
+  //   console.log(trackingList.length);
+  // }, [trackingInfo]);
+
   return (
     <Container>
       <SearchBar onSubmit={handleSubmit} />
       <ContainerInfo>
-        <TrackInfoBar />
-        <TrackHistory />
+        <TrackInfoBar trackingInfo={trackingInfo} />
+        <TrackHistory items={trackingList} onClick={onClick} />
       </ContainerInfo>
     </Container>
   );
