7a5a4747963d4b0a6a20fc8905c6809e9f75ddce
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2023-08-06 17:56:38 +0300

    Update Element.styled.js

--- src/styles/Element.styled.js
+++ src/styles/Element.styled.js
@@ -1,108 +1,109 @@
 import styled from 'styled-components';
-
 //import { Link } from 'react-router-dom';
-
-const Header = styled.header`
-  background-color: ${p => p.theme.colors.accent};
-  padding: 10px 20px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin: 0 auto;
-  width: 100%;
-  @media screen and (min-width: 768px) {
-    padding-left: 48px;
-    padding-right: 48px;
-  }
-`;
-
-const HeaderContainer = styled.div`
-  padding: 0 32px;
-`;
-
-const Container = styled.div`
-  padding: 16px 32px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`;
-
-const SearchBox = styled.div`
-  padding: 16px 32px;
-`;
-
-const ContainerInfo = styled.div`
-  padding: 16px 32px;
-  display: flex;
-  //flex-direction: column;
-  //justify-content: center;
-  gap: 30px;
-`;
 
 const LoaderContainer = styled.div`
   display: flex;
   justify-content: center;
 `;
 
-const HomeTitle = styled.h1`
-  color: hsl(269, 35%, 53%);
-  padding-top: 32px;
-  padding-bottom: 32px;
-  margin: auto;
-  text-align: center;
-`;
-const HomeContainer = styled.div`
-  padding-top: 36px;
+const Header = styled.header`
+  background-color: ${p => p.theme.colors.accent};
   display: flex;
-  align-items: center;
+  justify-content: space-between;
+  padding: 12px 24px;
+  width: 100%;
+`;
+
+const HeaderContainer = styled.div`
+  padding: 0 32px;
+  margin: 0 auto;
+  @media screen and (min-width: 768px) {
+    padding-left: 48px;
+    padding-right: 48px;
+  }
+  @media screen and (min-width: 1200px) {
+    padding-left: 48px;
+    padding-right: 48px;
+    max-width: 1200px;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
   flex-direction: column;
+  margin: 0 auto;
+  padding: 0 32px;
+  @media screen and (min-width: 768px) {
+    padding-left: 48px;
+    padding-right: 48px;
+  }
+  @media screen and (min-width: 1200px) {
+    padding-left: 48px;
+    padding-right: 48px;
+    max-width: 1200px;
+  }
 `;
 
-const SelectContainer = styled.div`
+const SearchBox = styled.div`
+  padding: 16px 32px;
+`;
+
+const Form = styled.form`
   display: flex;
   align-items: center;
-  justify-content: center;
-  margin-bottom: 24px;
-  gap: 10px;
+  width: 100%;
+  height: 30px;
+  //max-width: 300px;
+  background-color: #fff;
+  border-radius: 3px;
+  overflow: hidden;
+  margin-top: 8px;
 `;
 
-const SelectText = styled.p`
-  font-size: 18px;
-  font-weight: 600;
-  color: hsl(257, 46%, 27%);
+const Input = styled.input`
+  display: inline-block;
+  max-width: 300px;
+  font: inherit;
+  font-size: 16px;
+  outline: none;
+  border-radius: 8px;
+  padding: 4px;
+  &::placeholder {
+    font: inherit;
+    font-size: 16px;
+  }
 `;
-
-const SelectInput = styled.select`
-  width: 100px;
-  border-radius: 4px;
-  font-size: 14px;
-`;
-
-const List = styled.ul``;
-
-const ListItem = styled.li`
-  border-radius: ${p => p.theme.radii.card};
-  height: 24px;
-  margin-bottom: 8px;
-  font-weight: bold;
-`;
-
-const SelectButton = styled.button`
-  border-radius: ${p => p.theme.radii.card};
-  height: 24px;
-  border: none;
+const ButtonIcon = styled.button`
+  display: flex;
+  align-items: center;
   background-color: inherit;
-  font-weight: inherit;
+  width: 40px;
+  height: 40px;
+  border: 0;
+  margin-right: 4px;
+  margin-left: 4px;
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
   cursor: pointer;
-  &:first-of-type {
-    font-weight: bold;
+  outline: none;
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
+
+const ContainerInfo = styled.div`
+  padding: 16px 32px;
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
   }
 `;
 
 const ContentBox = styled.div`
   position: relative;
   padding: 20px;
-  flex-basis: calc((100% - 60px) / 2);
+  flex-basis: calc((100% - 30px) / 2);
   border: solid 1px black;
   border-radius: 8px;
 `;
@@ -122,6 +123,80 @@ const Text = styled.p`
 
 const TextInfo = styled.span`
   font-weight: normal;
+`;
+
+const ButtonDelete = styled.button`
+  position: absolute;
+  top: 10px;
+  right: 8px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: inherit;
+  width: 40px;
+  height: 40px;
+  border: 0;
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  cursor: pointer;
+  outline: none;
+  &:hover {
+    transform: scale(1.2);
+  }
+  @media screen and (min-width: 768px) {
+    top: 14px;
+    right: 16px;
+  }
+  @media screen and (min-width: 1200px) {
+    top: 14px;
+    right: 20px;
+  }
+`;
+
+const List = styled.ul``;
+
+const ListItem = styled.li`
+  border-radius: ${p => p.theme.radii.card};
+  height: 24px;
+  margin-bottom: 8px;
+  font-weight: bold;
+`;
+
+const Title = styled.h1`
+  color: hsl(269, 35%, 53%);
+  padding-top: 32px;
+  padding-bottom: 32px;
+  margin: auto;
+  text-align: center;
+`;
+const Galery = styled.ul`
+  padding-top: 30px;
+  margin-bottom: 30px;
+  @media screen and (min-width: 768px) {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+  }
+  @media screen and (min-width: 1200px) {
+  }
+`;
+
+const GaleryItem = styled.li`
+  margin-bottom: 20px;
+  padding: 12px;
+  border: solid 1px black;
+  border-radius: 8px;
+  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
+    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
+  &:last-child {
+    margin-bottom: 0;
+  }
+  @media screen and (min-width: 768px) {
+    flex-basis: calc((100% - 40px) / 3);
+    margin-bottom: 0;
+  }
+  @media screen and (min-width: 1200px) {
+    flex-basis: calc((100% - 60px) / 4);
+  }
 `;
 
 const Button = styled.button`
@@ -149,92 +224,6 @@ const Button = styled.button`
   }
 `;
 
-const Form = styled.form`
-  display: flex;
-  align-items: center;
-  width: 100%;
-  height: 30px;
-  //max-width: 300px;
-  background-color: #fff;
-  border-radius: 3px;
-  overflow: hidden;
-  margin-top: 8px;
-`;
-const Input = styled.input`
-  display: inline-block;
-  //width: 100%;
-  width: 300px;
-  font: inherit;
-  font-size: 16px;
-  // border: none;
-  outline: none;
-  border-radius: 8px;
-  padding: 4px;
-  &::placeholder {
-    font: inherit;
-    font-size: 16px;
-  }
-`;
-
-const ButtonIcon = styled.button`
-  display: flex;
-  align-items: center;
-  background-color: inherit;
-  width: 40px;
-  height: 40px;
-  border: 0;
-  margin-right: 4px;
-  margin-left: 4px;
-  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
-  cursor: pointer;
-  outline: none;
-  &:hover {
-    transform: scale(1.2);
-  }
-`;
-
-const ButtonDelete = styled.button`
-  position: absolute;
-  top: 20px;
-  right: 20px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: inherit;
-  width: 40px;
-  height: 40px;
-  border: 0;
-  margin-right: 4px;
-  margin-left: 4px;
-  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
-  cursor: pointer;
-  outline: none;
-  &:hover {
-    transform: scale(1.2);
-  }
-`;
-
-const Galery = styled.ul`
-  padding-top: 30px;
-  display: flex;
-  flex-wrap: wrap;
-  margin-bottom: 30px;
-  gap: 20px;
-  list-style: none;
-`;
-
-const GaleryItem = styled.li`
-  // display: block;
-  flex-basis: calc((100% - 60px) / 4);
-  padding: 12px;
-  //height: auto;
-  //overflow: hidden;
-  border: solid 1px black;
-  border-radius: 8px;
-  box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
-    0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
-`;
-
 export {
   Button,
   ButtonIcon,
@@ -245,8 +234,7 @@ export {
   GaleryItem,
   Header,
   HeaderContainer,
-  HomeContainer,
-  HomeTitle,
+  Title,
   Input,
   List,
   ListItem,
@@ -255,46 +243,5 @@ export {
   Text,
   TextInfo,
   SearchBox,
-  SelectContainer,
-  SelectInput,
-  SelectText,
-  SelectButton,
   ContentBox,
 };
-
-// const Button = styled.button`
-//   padding: 8px 16px;
-//   margin-top: ${p => p.theme.space[4]}px;
-//   border: none;
-//   border-radius: 15px;
-//   background-color: #3f51b5;
-//   color: white;
-
-//   text-decoration: none;
-//   font-family: inherit;
-//   font-size: 16px;
-//   line-height: 24px;
-//   font-style: normal;
-//   font-weight: 500;
-//   min-width: 180px;
-//   box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2),
-//     0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
-
-//   cursor: pointer;
-//   transition: background-color 250ms linear;
-//   &:hover {
-//     background-color: #303f9f;
-//   }
-// `;
-
-/*const P = styled.p`
-  color: ${p => p.theme.colors.black};
-  font-family: ${p => p.theme.fonts.body};
-  font-size: ${p => p.theme.fontSizes.m};
-  padding-top: 0px;
-  padding-bottom: 0px;
-  margin: 0px;
-  margin-top: ${p => p.theme.space[2]}px;
-  margin-bottom: ${p => p.theme.space[3]}px;
-`;
-*/
